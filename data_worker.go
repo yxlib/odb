38619beb1aeb130e610e1f9395d40c1a9c999b52
guanjianchang <guanjianchang@dianchu.com>
2022-12-20 14:12:18 +0800

    [odb] 1 Add DataWorker.DelCacheDatas() and DataWorker.DelCacheDataFields()

--- data_worker.go
+++ data_worker.go
@@ -44,153 +44,6 @@ const (
 	UPDATE_KEY_TAG_DEFAULT = "update_key"
 )
 
-type Cacheable interface {
-	FromCache(mapField2Val map[string]string) error
-	ToCache(fields []string) (map[string]interface{}, error)
-}
-
-type DBTableRow interface {
-	Cacheable
-}
-
-type BaseDBTableRow struct {
-}
-
-func (r *BaseDBTableRow) FromCache(mapField2Val map[string]string) error {
-	return nil
-}
-
-func (r *BaseDBTableRow) ToCache(fields []string) (map[string]interface{}, error) {
-	return nil, nil
-}
-
-func FromCache(r DBTableRow, mapField2Val map[string]string) error {
-	t := reflect.TypeOf(r).Elem()
-	v := reflect.ValueOf(r).Elem()
-
-	for i := 0; i < t.NumField(); i++ {
-		field := t.Field(i)
-		name := field.Tag.Get("mem")
-		if name == "" {
-			continue
-		}
-
-		strVal, ok := mapField2Val[name]
-		if ok {
-			value := v.FieldByName(field.Name)
-			fromCacheValue(value, strVal)
-		}
-	}
-
-	return nil
-}
-
-func fromCacheValue(value reflect.Value, strVal string) {
-	data := value.Interface()
-
-	switch data.(type) {
-	case bool:
-		boolVal, err := strconv.ParseBool(strVal)
-		if err == nil {
-			value.SetBool(boolVal)
-		}
-
-	case int, int8, int16, int32, int64:
-		intVal, err := strconv.ParseInt(strVal, 10, 64)
-		if err == nil {
-			value.SetInt(intVal)
-		}
-
-	case uint, uint8, uint16, uint32, uint64:
-		uintVal, err := strconv.ParseUint(strVal, 10, 64)
-		if err == nil {
-			value.SetUint(uintVal)
-		}
-
-	case float32, float64:
-		floatVal, err := strconv.ParseFloat(strVal, 64)
-		if err == nil {
-			value.SetFloat(floatVal)
-		}
-
-	case []byte:
-		value.SetBytes([]byte(strVal))
-
-	case string:
-		value.SetString(strVal)
-	}
-}
-
-func ToCache(r DBTableRow, fields []string) (map[string]interface{}, error) {
-	mapField2Val := make(map[string]interface{})
-
-	mapField2Flag := make(map[string]bool)
-	if len(fields) > 0 {
-		for _, field := range fields {
-			mapField2Flag[field] = true
-		}
-	}
-
-	t := reflect.TypeOf(r).Elem()
-	v := reflect.ValueOf(r).Elem()
-
-	for i := 0; i < t.NumField(); i++ {
-		field := t.Field(i)
-		name := field.Tag.Get("mem")
-		if name == "" {
-			continue
-		}
-
-		ok := (len(mapField2Flag) == 0)
-		if !ok {
-			_, ok = mapField2Flag[name]
-		}
-
-		if ok {
-			value := v.FieldByName(field.Name)
-			mapField2Val[name] = toCacheValue(value)
-		}
-	}
-
-	return mapField2Val, nil
-}
-
-func toCacheValue(value reflect.Value) string {
-	data := value.Interface()
-
-	switch v := data.(type) {
-	case bool:
-		boolVal := value.Bool()
-		return strconv.FormatBool(boolVal)
-
-	case int, int8, int16, int32, int64:
-		intVal := value.Int()
-		return strconv.FormatInt(intVal, 10)
-
-	case uint, uint8, uint16, uint32, uint64:
-		uintVal := value.Uint()
-		return strconv.FormatUint(uintVal, 10)
-
-	case float32, float64:
-		floatVal := value.Float()
-		return strconv.FormatFloat(floatVal, 'f', 5, 64)
-
-	case []byte:
-		return string(v)
-
-	case string:
-		return v
-	}
-
-	return ""
-}
-
-type CountRow struct {
-	BaseDBTableRow
-
-	Count int64 `db:"count"`
-}
-
 type DataWorker struct {
 	cacheDriver *CacheDriver
 	// mapCacheField2DbField map[string]string
@@ -528,6 +381,31 @@ func (w *DataWorker) ClearExpireCaches() error {
 
 	w.cacheDriver.Del(expireKeys...)
 	return nil
+}
+
+func (w *DataWorker) DelCacheDatas(keys ...string) (int64, error) {
+	delKeys := make([]string, 0)
+	for _, key := range keys {
+		cacheKey := w.getCacheKey(key)
+		delKeys = append(delKeys, cacheKey)
+	}
+
+	return w.cacheDriver.Del(delKeys...)
+}
+
+func (w *DataWorker) DelCacheDataFields(key string, fields ...string) (int64, error) {
+	cnt := int64(0)
+	cacheKey := w.getCacheKey(key)
+	for _, field := range fields {
+		_, err := w.cacheDriver.HDel(cacheKey, field)
+		if err != nil {
+			continue
+		}
+
+		cnt++
+	}
+
+	return cnt, nil
 }
 
 func (w *DataWorker) InsertToDb(mapper interface{}) (int64, error) {
